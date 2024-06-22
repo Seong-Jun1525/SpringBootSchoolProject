c433212ecd4f979ef7fca4eea7c4061b0cda0693
Jungmin <junminlee828@gmail.com>
2024-06-22 21:02:31 +0900

    공지사항 추가 했으나 홈에는 안뜨는 오류 있음 수정 못함

--- src/main/java/com/schoolproject/repository/GradeRepository.java
+++ src/main/java/com/schoolproject/repository/GradeRepository.java
@@ -3,11 +3,9 @@ package com.schoolproject.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-
 import com.schoolproject.entity.Grade;
-import com.schoolproject.entity.Student;
+
 
 @Repository
 public interface GradeRepository extends JpaRepository<Grade, Long> {
